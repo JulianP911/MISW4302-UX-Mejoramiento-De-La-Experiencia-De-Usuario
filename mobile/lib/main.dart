be59bd07e6d8673d833b63d4b12c670b57d0faab
Julian Padilla Molina <julianpadilla@tyba.com.co>
2024-09-18 14:43:48 -0500

    feat(mobile): Add new project for app mobile with flutter

---
+++ mobile/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:mobile/calendar_synchronization_view.dart';
+import 'package:mobile/events_with_alarms_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Up to date at work’s',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const CalendarSynchronizarionView(),
+        '/events_with_alarms': (context) => const EventsWithAlarmsView(),
+      },
+    );
+  }
+}
