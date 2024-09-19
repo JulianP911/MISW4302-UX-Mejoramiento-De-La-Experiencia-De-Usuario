e6b694c085a21fe3c0e9f2f517f4dff658992e6e
Julian Padilla Molina <julianpadilla@tyba.com.co>
2024-09-19 11:39:26 -0500

    feat(web): Add view for events list in web

--- web/src/router/router.tsx
+++ web/src/router/router.tsx
@@ -4,11 +4,13 @@ import {
   Route,
 } from "react-router-dom";
 import { LogIn } from "src/ui/screens/auth/logIn";
+import { EventWithAlarms } from "src/ui/screens/list/events";
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route>
       <Route path="/" element={<LogIn />} />
+      <Route path="/events-with-alarms" element={<EventWithAlarms />} />
     </Route>
   )
 );
